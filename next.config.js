29a6118b26df85e15b7613e8e9c1dc1f0e2d05aa
anis zouaghi <z.nissou43@gmail.com>
2025-11-22 00:05:12 +0100

    updated config files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+// module.exports = {
+//   images: {
+//     unoptimized: true,
+//     domains: [
+//       'res.cloudinary.com',
+//       'avatars.githubusercontent.com',
+//       'imgur.com',
+//     ],
+//   }
+// };
+
 module.exports = {
+  output: 'export',
   images: {
-    unoptimized: true,
-    domains: [
-      'res.cloudinary.com',
-      'avatars.githubusercontent.com',
-      'imgur.com',
-    ],
+    unoptimized: true
   }
-};
+}
