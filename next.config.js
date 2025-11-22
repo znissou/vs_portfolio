770ef8b68af196cc726f977d55b8b479df919c29
anis zouaghi <z.nissou43@gmail.com>
2025-11-22 16:48:57 +0100

    integrated the package of cloudflare for deploying the web app to a worker

--- next.config.js
+++ next.config.js
@@ -9,9 +9,28 @@
 //   }
 // };
 
+// module.exports = {
+//   output: 'export',
+//   images: {
+//     unoptimized: true
+//   }
+// }
+
 module.exports = {
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
+  output: 'export'
 }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
